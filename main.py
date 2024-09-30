143f5fd82b132eab2c0e952add7a06c812b3d79f
abra-abe <abewills17@gmail.com>
2024-09-30 23:35:27 +0300

    add author, add book and fetch book endpoints...

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, Annotated
+from sqlmodel import Session, select
+from db import get_session, init_db
+from models import Author, Book
+
+app = FastAPI()
+
+# initialize db
+init_db()
 
 # pydantic models
 class AuthorCreate(BaseModel):
@@ -16,8 +24,43 @@ class BookUpdate(BaseModel):
     publication_year: Optional[int]
     author_id: Optional[int]
 
-app = FastAPI()
+db_dependency = Annotated[Session, Depends(get_session)]
 
 @app.get("/")
 def home():
     return {"message": "welcome to the home directory"}
+
+# create authors route
+@app.post("/add-author")
+def create_authors(author: AuthorCreate, session: db_dependency):
+    db_author = Author(name = author.name)
+    session.add(db_author)
+    session.commit()
+    session.refresh(db_author)
+    return db_author
+
+# create book route
+@app.post("/add-book")
+def create_book(book: BookCreate, session: db_dependency):
+    db_book = Book(title=book.title, publication_year=book.publication_year, author_id=book.author_id)
+    session.add(db_book)
+    session.commit()
+    session.refresh(db_book)
+    return db_book
+
+# get books route
+@app.get("/fetch-books")
+def fetch_books(session: db_dependency):
+    statement = select(Book)
+    books = session.exec(statement).all()
+    return [
+        {
+            "title": book.title,
+            "publication_year": book.publication_year,
+            "author": {
+                "id": book.author_id,
+                "name": book.author.name
+            }
+        }
+        for book in books
+    ]
